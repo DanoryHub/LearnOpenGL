ee76025620f9800fc6874437ed8549b8791ea035
Ivan Miatselski <danorywork@gmail.com>
2025-09-14 23:15:16 +0300

    Add vertex buffer object generate, bind and fill with vertices

--- src/HelloTriangle/HelloTriangle.cpp
+++ src/HelloTriangle/HelloTriangle.cpp
@@ -11,6 +11,16 @@ int RenderTriangle()
 	initGLFW(4, 4);
 	GLFWwindow* window = createWindow("HelloWindow");
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, -0.5f, 0.0f,
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glfwMakeContextCurrent(window);
 
