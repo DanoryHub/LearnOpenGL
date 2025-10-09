282f4f4dd226696d7cd1a7784cfc8ab1d36ecf0c
Ivan Miatselski <danorywork@gmail.com>
2025-10-09 22:17:26 +0300

    Added doubled textures

--- src/HelloTriangle/HelloTriangle.cpp
+++ src/HelloTriangle/HelloTriangle.cpp
@@ -80,9 +80,18 @@ int RenderTriangle()
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    unsigned int textures[2];
+
+    const char *texturePaths[2][255] = {
+        { "C:\\Users\\IvanMiatselski\\Desktop\\LearnOpenGL\\src\\HelloTriangle\\Textures\\container.jpg" },
+        { "C:\\Users\\IvanMiatselski\\Desktop\\LearnOpenGL\\src\\HelloTriangle\\Textures\\awesomeface.png" }
+    };
+
+    stbi_set_flip_vertically_on_load(true);
+
+    // Declare parameters for first texture
+    glGenTextures(1, &textures[0]);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -90,20 +99,46 @@ int RenderTriangle()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     int width, height, nrChannels;
-    unsigned char *data = stbi_load("C:\\Users\\IvanMiatselski\\Desktop\\LearnOpenGL\\src\\HelloTriangle\\Textures\\container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *textureData = stbi_load(*texturePaths[0], &width, &height, &nrChannels, 0);
 
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    if (textureData) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else {
         std::cout << "Failed to load an image texture." << std::endl;
     }
-    stbi_image_free(data);
+    stbi_image_free(textureData);
+
+    // Declare parameters for second texture
+    glGenTextures(1, &textures[1]);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    textureData = stbi_load(*texturePaths[1], &width, &height, &nrChannels, 0);
+
+    if (textureData) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load an image texture." << std::endl;
+    }
+
+    stbi_image_free(textureData);
 
     // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // GL_FILL or GL_LINE
     //
     // float horizontalOffset = 0.0f;
+
+    baseShader.use();
+
+    baseShader.setInt("texture1", 0);
+    baseShader.setInt("texture2", 1);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -112,12 +147,14 @@ int RenderTriangle()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
 
         // float timeValue = glfwGetTime();
         // float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         // int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        baseShader.use();
         // baseShader.setFloat("horizontalOffset", horizontalOffset);
         // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
